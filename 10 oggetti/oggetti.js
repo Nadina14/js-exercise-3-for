1a6fcbef452e843654484618e328aef50cabfd79
Nadina <sheriefnadina@gmail.com>
2023-10-25 12:25:56 +0200

    feat / consegne copiate

---
+++ 10 oggetti/oggetti.js
@@ -0,0 +1,3 @@
+/* Crea un array di 10 oggetti che rappresentano una mela, indicando per ognuna varietà, peso e diametro.
+Calcola quanto pesano tutte le mele.
+BONUS Dividi in due array separati le mele con diametro maggiore di 10cm. Infine stampa separatamente quanto pesano i due gruppi di mele. */
---
+++ casual 1-100/casual.js
@@ -0,0 +1,2 @@
+/* Fai inserire un numero, che chiameremo N, all’utente.
+Genera N array, ognuno formato da 10 numeri casuali tra 1 e 100. */
