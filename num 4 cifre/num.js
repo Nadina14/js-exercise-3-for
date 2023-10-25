9618562b7ae418c34e45beeab8ebe0ed7a35798e
Nadina <sheriefnadina@gmail.com>
2023-10-25 12:44:48 +0200

    feat/ esercizio num a 4 cifre

--- num 4 cifre/num.js
+++ num 4 cifre/num.js
@@ -1,2 +1,16 @@
 /* Chiedi un numero di 4 cifre all’utente
 e calcola la somma di tutte le cifre che compongono il numero. */
+
+let numString = prompt('Scrivi un numero intero di 4 cifre');
+let numN = parseInt(numString);
+
+if (isNaN(numN) || numN.toString().length !== 4){
+    alert('Non hai inserito un numero intero di 4 cifre');
+}else {
+    let somma = 0;
+    for(let i = 0; i < numString.length; i++){
+        let char = numString[i];
+        somma += Number(char);
+    }
+    alert('La somma dei numeri che hai inserito è ' + somma);
+}
